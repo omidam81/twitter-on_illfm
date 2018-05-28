22bcefc7bbfc9730980ab856d309c0cf3f4b9e84
Omid Amiri <omid@Omids-MacBook-Pro.local>
2018-05-29 01:46:12 +0430

    changes;

--- index.js
+++ index.js
@@ -19,8 +19,30 @@ app.use(bodyParser.urlencoded({
 
 function fetchData(callback) {
     http.get("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + user + "&api_key=" + key + "&format=json", (res) => {
-        conole.log(res);
-        callback();
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            try {
+                const parsedData = JSON.parse(rawData);
+                //console.log(rawData);
+                var currentTrack = {};
+                var tracks = parsedData.recenttracks.track;
+
+                for (let i = tracks.length - 1; i >= 0; i--) {
+                    let t = tracks[i];
+                    if (t['@attr'] && t['@attr'].nowplaying) {
+                        currentTrack.name = t.name;
+                        currentTrack.url = t.url;
+                        currentTrack.artist = t.artist["#text"];
+                    }
+                }
+
+                console.log(currentTrack);
+
+            } catch (e) {
+                console.error(e.message);
+            }
+        });
     });
 }
 
